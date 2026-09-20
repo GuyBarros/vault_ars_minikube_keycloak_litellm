28a7985ec99d909a9840345c88aafb90291800a8
Guy Barros <guy@Hashicorp.com>
2026-09-20 19:08:17 -0300

    consul metrics

--- infra/local-minikube/local-config.sh
+++ infra/local-minikube/local-config.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Local-only settings for the minikube workflow.
+# When set, bootstrap.sh skips minikube driver auto-detection and uses this.
+MINIKUBE_DRIVER="podman"
 CONSUL_IMAGE_VERSION="2.0.1-ent"
 VAULT_IMAGE_VERSION="2.1.0-ent"
 POSTGRES_ADMIN_PASSWORD="HashiCorp123!"
